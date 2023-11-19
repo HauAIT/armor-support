0a3e75a53077016c3820da27c0205652d386de54
duy manh <duym171003@gmail.com>
2023-11-19 11:11:40 +0700

    Update path libs

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-import * as tempDir from './build/lib/tempdir';
-import * as system from './build/lib/system';
-import * as util from './build/lib/util';
-import * as fsIndex from './build/lib/fs';
-import * as net from './build/lib/net';
-import * as plist from './build/lib/plist';
-import * as mkdirpIndex from './build/lib/mkdirp';
-import * as logger from './build/lib/logging';
-import * as process from './build/lib/process';
-import * as zip from './build/lib/zip';
-import * as imageUtil from './build/lib/image-util';
-import * as mjpeg from './build/lib/mjpeg';
-import * as node from './build/lib/node';
-import * as timing from './build/lib/timing';
+module.exports = require('./build/lib');
 
-
-const { fs } = fsIndex;
-const { cancellableDelay } = util;
-const { mkdirp } = mkdirpIndex;
-
-export {
-  tempDir, system, util, fs, cancellableDelay, plist, mkdirp, logger, process,
-  zip, imageUtil, net, mjpeg, node, timing,
-};
-export default {
-  tempDir, system, util, fs, cancellableDelay, plist, mkdirp, logger, process,
-  zip, imageUtil, net, mjpeg, node, timing,
-};
+// import * as tempDir from './build/lib/tempdir';
+// import * as system from './build/lib/system';
+// import * as util from './build/lib/util';
+// import * as fsIndex from './build/lib/fs';
+// import * as net from './build/lib/net';
+// import * as plist from './build/lib/plist';
+// import * as mkdirpIndex from './build/lib/mkdirp';
+// import * as logger from './build/lib/logging';
+// import * as process from './build/lib/process';
+// import * as zip from './build/lib/zip';
+// import * as imageUtil from './build/lib/image-util';
+// import * as mjpeg from './build/lib/mjpeg';
+// import * as node from './build/lib/node';
+// import * as timing from './build/lib/timing';
+//
+//
+// const { fs } = fsIndex;
+// const { cancellableDelay } = util;
+// const { mkdirp } = mkdirpIndex;
+//
+// export {
+//   tempDir, system, util, fs, cancellableDelay, plist, mkdirp, logger, process,
+//   zip, imageUtil, net, mjpeg, node, timing,
+// };
+// export default {
+//   tempDir, system, util, fs, cancellableDelay, plist, mkdirp, logger, process,
+//   zip, imageUtil, net, mjpeg, node, timing,
+// };
